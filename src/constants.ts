9f5aeb801052724690cd737bff735c4de9172585
Amit Mahato <amit643.mahato@gmail.com>
2022-10-15 15:04:01 +0545

    4.1 Writing the framework - Define list of environment variables from different providers - Copy the compiled & deployed TwitterDapp smart contract ABI to contract-abi.json - Define constants based on the environment variables - Define an isolated class to use all the methods available on the smart contract using all required configurations - Define App.tsx with UI components and use Web3Auth & Torus Wallet

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+export const APP_CONSTANTS = {
+  ALCHEMY_KEY: process.env.REACT_APP_ALCHEMY_KEY,
+  CONTRACT_ADDRESS: process.env.REACT_APP_CONTRACT_ADDRESS,
+  OPENSEA_ASSETS_URL: "https://testnets.opensea.io/assets/mumbai",
+  CLIENT_ID: process.env.REACT_APP_CLIENT_ID,
+  RPC_TARGET: process.env.REACT_APP_RPC_TARGET,
+  APP_LOGO: "https://i.ibb.co/2gmPw9f/twitter-Dapp-Logo.jpg",
+  ADAPTER_TWITTER_CLIENT_VERIFIER:
+    process.env.REACT_APP_ADAPTER_TWITTER_CLIENT_VERIFIER,
+  ADAPTER_TWITTER_CLIENT_ID: process.env.REACT_APP_ADAPTER_TWITTER_CLIENT_ID,
+  AUTH0_DOMAIN: process.env.REACT_APP_AUTH0_DOMAIN,
+  BIRD_IMAGE: "https://i.ibb.co/42WNxRN/reshot-icon-bird-TL39-D5-P8-YH.png",
+  REACT_APP_REFRESH_TIMER: 30,
+};
